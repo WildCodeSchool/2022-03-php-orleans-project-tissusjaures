33a8d2ad77158e1b681c36f3ef139f433e2802e1
Ysedda <arthur.baudoin@icloud.com>
2022-04-20 16:02:58 +0200

    insert model done

--- src/Model/ClothManager.php
+++ src/Model/ClothManager.php
@@ -11,8 +11,15 @@ class ClothManager extends AbstractManager
      */
     public function insert(array $cloth): int
     {
-        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`name`) VALUES (:name)");
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`name`, `price`, 
+        `image`, `is_on_sale`, `is_new`, `cloth_categories_id`) 
+        VALUES (:name, :price, :image, :is_on_sale, :is_new, :cloth_categories_id)");
         $statement->bindValue('name', $cloth['name'], \PDO::PARAM_STR);
+        $statement->bindValue('price', $cloth['price'], \PDO::PARAM_STR);
+        $statement->bindValue('image', $cloth['image'], \PDO::PARAM_STR);
+        $statement->bindValue('is_on_sale', $cloth['is_on_sale'], \PDO::PARAM_BOOL);
+        $statement->bindValue('is_new', $cloth['is_new'], \PDO::PARAM_BOOL);
+        $statement->bindValue('cloth_categories_id', $cloth['cloth_categories_id'], \PDO::PARAM_INT);
 
         $statement->execute();
         return (int)$this->pdo->lastInsertId();
