375caf6cd1f31dca3cd32049d5a2121e5e88d205
Chloebert <berthier.chloe@wanadoo.fr>
2022-04-22 11:40:43 +0200

    détails

--- src/Model/HomeClothManager.php
+++ src/Model/HomeClothManager.php
@@ -4,7 +4,7 @@ namespace App\Model;
 
 class HomeClothManager extends AbstractManager
 {
-    public const TABLE = 'cloth'; 
+    public const TABLE = 'cloth';
 
         /**
      * Get one row from database by isNew.
--- src/Model/HomeMachineManager.php
+++ src/Model/HomeMachineManager.php
@@ -4,7 +4,7 @@ namespace App\Model;
 
 class HomeMachineManager extends AbstractManager
 {
-    public const TABLE = 'machines'; 
+    public const TABLE = 'machines';
 
         /**
      * Get one row from database by isNew.
