6d1b4d370787e81c711fb5e15215f7079d133deb
baloolc <viann45@hotmail.fr>
2022-04-29 09:38:34 +0200

    resolve conflict

--- src/routes.php
+++ src/routes.php
@@ -16,5 +16,5 @@ return [
     'admin/machines' => ['AdminMachineController', 'index',],
     'admin/machines/ajouter' => ['AdminMachineController', 'addMachine',],
     'admin/machines/supprimer' => ['AdminMachineController', 'deleteMachine',],
-    'tips' => ['TipsController', 'showMonthlyTip'],
+    'Astuces' => ['TipsController', 'showMonthlyTip'],
 ];
