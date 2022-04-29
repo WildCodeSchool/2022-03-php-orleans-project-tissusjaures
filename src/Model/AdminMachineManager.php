1ec97eeafb22f28520e118b8a4a7ddf49ed9c5cc
JS.Derouet <js.derouet@sfr.fr>
2022-04-29 11:24:42 +0200

    EditMachine 1.2

--- src/Model/AdminMachineManager.php
+++ src/Model/AdminMachineManager.php
@@ -6,16 +6,6 @@ class AdminMachineManager extends AbstractManager
 {
     public const TABLE = 'machines';
 
-    public function selectOneById(int $id): array|false
-    {
-        $statement = $this->pdo->prepare("SELECT m.name, m.price, m.description FROM " . static::TABLE . " AS m"
-        . " WHERE m.id=:id");
-        $statement->bindValue('id', $id, \PDO::PARAM_INT);
-        $statement->execute();
-
-        return $statement->fetch();
-    }
-
     public function insert(array $machine): int
     {
         $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`name`, `price`, 
@@ -32,7 +22,7 @@ class AdminMachineManager extends AbstractManager
     public function update(array $machine): bool
     {
         $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `name` = :name, `price` = :price, 
-        `description` = :description, WHERE id=:id");
+        `description` = :description WHERE id=:id");
         $statement->bindValue('id', $machine['id'], \PDO::PARAM_INT);
         $statement->bindValue('name', $machine['name'], \PDO::PARAM_STR);
         $statement->bindValue('price', $machine['price'], \PDO::PARAM_STR);
