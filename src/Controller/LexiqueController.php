85ec1c0e5fcbac914c507289828f388e0c2c487f
Daniel ESCARAVAGE <escaravage.daniel@gmail.com>
2022-05-03 18:51:00 +0200

    on continue sur le style ça commence à être solide

--- src/Controller/LexiqueController.php
+++ src/Controller/LexiqueController.php
@@ -3,14 +3,22 @@
 namespace App\Controller;
 
 use App\Model\LexiqueManager;
+use App\Model\TipCategoryManager;
 
 class LexiqueController extends AbstractController
 {
     public function index(): string
     {
+        $tipCategoryManager = new TipCategoryManager();
+        $tipCategories = $tipCategoryManager->selectAll();
+
         $lexiqueManager = new LexiqueManager();
         $lexiques = $lexiqueManager->selectAll();
 
-        return $this->twig->render('Lexique/index.html.twig', ['lexiques' => $lexiques]);
+
+        return $this->twig->render('Lexique/index.html.twig', [
+            'lexiques' => $lexiques,
+            'tipCategories' => $tipCategories,
+        ]);
     }
 }
