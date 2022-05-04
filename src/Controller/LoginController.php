9d00fc649ed8cf7a231a3fbb8437cf14531ed1cb
Daniel ESCARAVAGE <escaravage.daniel@gmail.com>
2022-05-04 14:45:06 +0200

    debut de sécurisaation du formulaire de login

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -7,7 +7,28 @@ class LoginController extends AbstractController
     public function login(): string
     {
         if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            $connexion = array_map('trim', $_POST);
+            $errors = $this->validate($connexion);
         }
-        return $this->twig->render('Login/login.html.twig');
+        return $this->twig->render('Login/login.html.twig', [
+            'errors' => $errors,
+        ]);
+    }
+
+    private function validate(array $connexion): array
+    {
+        if (empty($connexion['email'])) {
+            $errors[] = 'Le champ email ne doit pas être vide';
+        }
+        if (empty($connexion['password'])) {
+            $errors[] = 'Le mot de passe ne doit pas être vide';
+        }
+        if (!filter_var($connexion['email'], FILTER_VALIDATE_EMAIL)) {
+            $errors[] = "format d'email invalide";
+        }
+        if (empty($errors)) {
+            //tentative de login
+        }
+        return $errors;
     }
 }
