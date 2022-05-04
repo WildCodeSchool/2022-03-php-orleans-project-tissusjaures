4ce7faa2b71e2a58ba0a03807f769bb835611b6c
Chloebert <berthier.chloe@wanadoo.fr>
2022-05-04 12:29:07 +0200

    méthode edit dans controller et manager

--- src/Controller/AdminClothCategoryController.php
+++ src/Controller/AdminClothCategoryController.php
@@ -13,4 +13,39 @@ class AdminClothCategoryController extends AbstractController
 
         return $this->twig->render('Admin/ClothCategory/index.html.twig', ['clothCategories' => $clothCategories]);
     }
+
+    // public function editClothCategory($id): string
+    // {
+    //     $categoryErrors = [];
+    //     $imageErrors = [];
+
+    //     $clothCategoryList = new ClothCategoryManager();
+    //     $clothCategories = $clothCategoryList->selectOneById($id);
+
+    //     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    //         $clothCategories = array_map('trim', $_POST);
+    //         $clothCategories['id'] = $id;
+            
+    //         $categoryErrors = $this->clothCategoryValidate($clothCategoryList);
+
+    //         $imageFile = $_FILES['image'];
+    //         $imageErrors = $this->validateImage($imageFile);
+
+    //         if (empty($categoryErrors) && empty($imageErrors)) {
+    //             $extension = pathinfo($imageFile['name'], PATHINFO_EXTENSION);
+    //             $imageName = uniqid('', true) . '.' . $extension;
+
+    //             move_uploaded_file($imageFile['tmp_name'], UPLOAD_PATH . '/' . $imageName);
+
+    //             $clothCategoryManager = new ClothCategoryManager();
+    //             $clothCategoryList['image'] = $imageName;
+    //             $clothCategoryList->update($clothCategories);
+    //             header('Location: /admin/categories-tissus/');
+    //         }
+    //     }
+    //     return $this->twig->render('Admin/ClothCategory/add.html.twig', [
+    //         'categoryErrors' => $categoryErrors,
+    //         'imageErrors' => $imageErrors,
+    //     ]);
+    // }
 }
--- src/Model/ClothCategoryManager.php
+++ src/Model/ClothCategoryManager.php
@@ -5,4 +5,15 @@ namespace App\Model;
 class ClothCategoryManager extends AbstractManager
 {
     public const TABLE = 'cloth_categories';
+
+    // public function update(array $clothCategory): bool
+    // {
+    //     $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `name` = :name, `price` = :price, 
+    //     `cloth_categories_id` = :cloth_categories_id WHERE id=:id");
+    //     $statement->bindValue('id', $clothCategory['id'], \PDO::PARAM_INT);
+    //     $statement->bindValue('name', $clothCategory['name'], \PDO::PARAM_STR);
+    //     $statement->bindValue('image', $clothCategory['price'], \PDO::PARAM_STR);
+
+    //     return $statement->execute();
+    // }
 }
