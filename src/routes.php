3d256ef69298c0accaa2471890328f47c92ae72b
JS.Derouet <js.derouet@sfr.fr>
2022-05-04 14:37:16 +0200

    fix Maj

--- src/routes.php
+++ src/routes.php
@@ -19,7 +19,7 @@ return [
     'admin/machines/ajouter' => ['AdminMachineController', 'addMachine',],
     'admin/machines/editer' => ['AdminMachineController', 'editMachine', ['id']],
     'admin/machines/supprimer' => ['AdminMachineController', 'deleteMachine',],
-    'admin/Lexicon/ajouter' => ['AdminLexiconController', 'addLexicon'],
+    'admin/lexicon/ajouter' => ['AdminLexiconController', 'addLexicon'],
     'astuces' => ['TipsController', 'showTips'],
     'contact' => ['ContactController', 'index',],
     'admin/categories-tissus' => ['AdminClothCategoryController', 'index',],
