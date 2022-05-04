f2e83e1445a260076b0ffc83b13c4775bc6f4936
Daniel ESCARAVAGE <escaravage.daniel@gmail.com>
2022-05-04 12:02:39 +0200

    debut de qlq chose

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -7,8 +7,7 @@ class LoginController extends AbstractController
     public function login(): string
     {
         if ($_SERVER["REQUEST_METHOD"] === "POST") {
-            
-        }    
+        }
         return $this->twig->render('Login/login.html.twig');
     }
 }
