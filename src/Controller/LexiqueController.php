f6008f1f996535f60f036c8eb4b09e80299b9dd6
Daniel ESCARAVAGE <escaravage.daniel@gmail.com>
2022-05-04 09:18:19 +0200

    encore et toujours des petites modifications

--- src/Controller/LexiqueController.php
+++ src/Controller/LexiqueController.php
@@ -14,7 +14,6 @@ class LexiqueController extends AbstractController
         $lexiqueManager = new LexiqueManager();
         $lexiques = $lexiqueManager->selectAll();
 
-
         return $this->twig->render('Lexique/index.html.twig', [
             'lexiques' => $lexiques,
             'tipCategories' => $tipCategories,
