05d3587da7c494273f86c82fb6557924e04fc45f
Ysedda <arthur.baudoin@icloud.com>
2022-05-05 09:52:41 +0200

    created manager and controller

--- src/Model/TipManager.php
+++ src/Model/TipManager.php
@@ -21,4 +21,19 @@ class TipManager extends AbstractManager
 
         return $statement->fetchAll();
     }
+
+    public function insert(array $tip): int
+    {
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`name`, `image`, `content`
+        , `is_monthly_tip`, `tips_and_tricks_categories_id`) 
+        VALUES (:name, :image, :content, :is_monthly_tip, :tips_and_tricks_categories_id)");
+        $statement->bindValue('name', $tip['name'], \PDO::PARAM_STR);
+        $statement->bindValue('image', $tip['image'], \PDO::PARAM_STR);
+        $statement->bindValue('content', $tip['content'], \PDO::PARAM_STR);
+        $statement->bindValue('is_monthly_tip', $tip['is_monthly_tip'], \PDO::PARAM_INT);
+        $statement->bindValue('tips_and_tricks_categories_id', $tip['tips_and_tricks_categories_id'], \PDO::PARAM_INT);
+
+        $statement->execute();
+        return (int)$this->pdo->lastInsertId();
+    }
 }
