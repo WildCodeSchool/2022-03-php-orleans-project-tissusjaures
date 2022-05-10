f1b9cd5b3d149b981f95ced69a039c4c5f26f4dc
JS.Derouet <js.derouet@sfr.fr>
2022-05-10 11:10:13 +0200

    before the merge 3.1

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+class AdminController extends AbstractController
+{
+    //public function index(): string
+    //{
+        //$user = $this->getUser();
+        //return $this->twig->render('Admin/Home/index.html.twig', [
+        //    'user' => $user]);
+    //}
+}
