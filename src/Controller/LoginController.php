a60c4975340974f05aaa4e66f72f3c302012e22f
JS.Derouet <js.derouet@sfr.fr>
2022-05-11 07:43:49 +0200

    resolve error

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -20,7 +20,7 @@ class LoginController extends AbstractController
                 if ($user) {
                     if (password_verify($connection['password'], $user['password'])) {
                         $_SESSION['user'] = $user['id'];
-                        header('Location: /admin/categories-tissus');
+                        header('Location: /admin/');
                     } else {
                         $errors[] = 'Mauvais identifiants';
                     }
